39ea3eae4b0d466545492a429220f4f3ade61819
Grace Colette <ilovegrace09@gmail.com>
2019-06-20 21:04:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   words = sentence.split(",")
-  puts words.collect do |words| words.reverse
+  return words.collect do |words| words.reverse
 end
 return words
 end
